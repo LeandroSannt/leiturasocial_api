22bbbeaba0bb0c0278a838e5040b1c88fa2f2a02
LeandroSannt <lsn_slim@yahoo.com.br>
2022-06-13 14:28:26 -0300

    ajustes

--- dist/config/auth.js
+++ dist/config/auth.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     jwt: {
         secret: '2abd2d55b3ddafdf2a9050476c19f938',
-        expiresIn: '10d'
+        expiresIn: '1d'
     }
 };
